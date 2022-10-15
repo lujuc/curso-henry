bca57e71730f32b722969a1e41739497c42a674f
lujuc <juniorcruz.c@gmail.com>
2022-10-14 21:38:55 -0500

    modificado

--- primerEjemplo.js
+++ primerEjemplo.js
@@ -1,5 +1,6 @@
-let num1;
-let num2;
-let result=0;
-result = num1+num2;
-console.log(result);
+let num1; //variable para numero1
+let num2;// variable para numero2
+let result=0;// almacena el resultado
+result = num1+num2;// hace la operacion de suma
+console.log(result);// muestra el resultado en consola
+//algo mas de codigo
